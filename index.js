0ae52eb2695d1f2cda5c603f966a9d1f234b174e
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-25 11:10:12 +0200

    More initial setup

--- index.js
+++ index.js
@@ -2,14 +2,21 @@
 
 const getNextJobFromQueue = require('./lib/get-next-job-from-queue')
 const getFileData = require('./lib/get-file-data')
+const saveToNotifications = require('./lib/save-to-notifications')
+const prepareDocument = require('./lib/prepare-document')
 const saveToDone = require('./lib/save-to-done')
 const logger = require('./lib/logger')
 
+logger('info', ['index', 'start'])
+
 getNextJobFromQueue()
   .then(getFileData)
+  .then(saveToNotifications)
+  .then(prepareDocument)
   .then(saveToDone)
   .then((data) => {
     logger('info', ['index', data._id, 'finished'])
+    process.exit(0)
   })
   .catch((error) => {
     logger('error', ['index', 'error', JSON.stringify(error)])
