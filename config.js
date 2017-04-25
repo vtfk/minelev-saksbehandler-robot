f27c9cac7a638ccc7bc767926ae6dcc73d3cccbe
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-25 09:03:02 +0200

    More basic setup

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 'use strict'
 
 module.exports = {
-  CONVERT_DIRECTORY_PATH: process.env.CONVERT_DIRECTORY_PATH || 'test/data/convert',
-  DONE_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/data/done',
-  ERRORS_DIRECTORY_PATH: process.env.ERRORS_DIRECTORY_PATH || 'test/data/errors',
-  QUEUE_DIRECTORY_PATH: process.env.QUEUE_DIRECTORY_PATH || 'test/data/queue'
+  CALLBACK_DIRECTORY_PATH: process.env.CALLBACK_DIRECTORY_PATH || 'test/directories/callback',
+  CONVERT_DIRECTORY_PATH: process.env.CONVERT_DIRECTORY_PATH || 'test/directories/convert',
+  DONE_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/directories/done',
+  ERRORS_DIRECTORY_PATH: process.env.ERRORS_DIRECTORY_PATH || 'test/directories/errors',
+  NOTIFICATIONS_DIRECTORY_PATH: process.env.NOTIFICATIONS_DIRECTORY_PATH || 'test/directories/notifications',
+  QUEUE_DIRECTORY_PATH: process.env.QUEUE_DIRECTORY_PATH || 'test/directories/queue',
+  PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'minelev-robot',
+  PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
+  PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
 }
--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 const getNextJobFromQueue = require('./lib/get-next-job-from-queue')
 const getFileData = require('./lib/get-file-data')
+const saveToDone = require('./lib/save-to-done')
 const logger = require('./lib/logger')
 
 getNextJobFromQueue()
   .then(getFileData)
+  .then(saveToDone)
   .then((data) => {
     logger('info', ['index', data._id, 'finished'])
   })
