6721ea85fefc7660ea6662012d29901f265eed2a
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-05-05 14:10:32 +0200

    Various updates

--- config.js
+++ config.js
@@ -7,6 +7,8 @@ module.exports = {
   ERRORS_DIRECTORY_PATH: process.env.ERRORS_DIRECTORY_PATH || 'test/directories/errors',
   NOTIFICATIONS_DIRECTORY_PATH: process.env.NOTIFICATIONS_DIRECTORY_PATH || 'test/directories/notifications',
   QUEUE_DIRECTORY_PATH: process.env.QUEUE_DIRECTORY_PATH || 'test/directories/queue',
+  JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
+  DSF_SERVICE_URL: process.env.DSF_SERVICE_URL || 'https://dsf.mikrotjenester.win',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'minelev-robot',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
