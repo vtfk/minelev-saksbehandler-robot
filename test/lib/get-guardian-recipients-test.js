d7455cf37c8fbafb689249359b57c18ed1080cfe
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-05-09 12:15:07 +0200

    Implements get-guardian-recipients

---
+++ test/lib/get-guardian-recipients-test.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const test = require('ava')
+const getGuardianRecipients = require('../../lib/get-guardian-recipients')
+
+test('Returns 2 for Delt', t => {
+  const parents = require('../data/parentsDelt.json')
+  const recipients = getGuardianRecipients(parents)
+  t.is(recipients.length, 2, '2 recipients returned')
+})
+
+test('Returns 1 for Felles', t => {
+  const parents = require('../data/parentsFelles.json')
+  const recipients = getGuardianRecipients(parents)
+  t.is(recipients.length, 1, '1 recipient returned')
+})
