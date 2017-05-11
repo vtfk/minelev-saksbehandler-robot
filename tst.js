508a1c130add6a3b5852352a7d65318139598273
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-05-11 11:01:40 +0200

    Implements guardians and restricted address

--- tst.js
+++ tst.js
@@ -1,6 +1,9 @@
 'use strict'
 
-const filterGuardian = require('./lib/filter-guardian')
+const normalize = require('tfk-dsf-normalize-contact')
+// const filterGuardian = require('./lib/filter-guardian')
 const felles = require('./test/data/hentForeldreFelles.json')['RESULT']
 
-console.log(filterGuardian(felles))
+// console.log(filterGuardian(felles))
+
+console.log(normalize(felles.HOV))
