c7a3a6b7b3e94a9724e435d00d0a63037dacad7b
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-08-21 10:43:29 +0200

    Adds parents as contacts for archive

--- tst.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const normalize = require('tfk-dsf-normalize-contact')
-// const filterGuardian = require('./lib/filter-guardian')
-const felles = require('./test/data/hentForeldreFelles.json')['RESULT']
-
-// console.log(filterGuardian(felles))
-
-console.log(normalize(felles.HOV))
