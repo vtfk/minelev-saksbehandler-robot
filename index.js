c536cacd21aca884c978d18c0573421c042eaf79
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-21 21:05:23 +0100

    Added convertion from new to old document schema

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const getNextJobFromQueue = require('./lib/steps/get-next-job-from-queue')
 const getFileData = require('./lib/steps/get-file-data')
 const decryptData = require('./lib/steps/decrypt-data')
+const convertData = require('./lib/steps/convert-document')
 const decideFlow = require('./lib/steps/decide-flow')
 const  { logger } = require('@vtfk/logger')
 
@@ -9,6 +10,7 @@ logger('info', ['index', 'start'])
 getNextJobFromQueue()
   .then(getFileData)
   .then(decryptData)
+  .then(convertData)
   .then(decideFlow)
   .then(data => {
     logger('info', ['index', data._id, 'finished'])
