69717592c1de0b43be1618e09dd070de90afa467
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-08-09 09:28:33 +0200

    Added test for `too short fnr`

--- test/lib/test-valid-vgo-number.js
+++ test/lib/test-valid-vgo-number.js
@@ -10,3 +10,12 @@ test('Invalid VGO number', t => {
   const fnr = '26118645108'
   t.false(isValidVgoNumber(fnr), 'false found')
 })
+
+test('Invalid VGO number (too short)', t => {
+  const fnr = '2611864510'
+  try {
+    isValidVgoNumber(fnr)
+  } catch (error) {
+    t.true(error.message === 'Input must be 11 digits', 'Too short fnr throws "Input must be 11 digits"')
+  }
+})
